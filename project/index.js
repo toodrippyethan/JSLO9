6e41d4ff2eb1168f2c037c45687c7228d8cdd373
Ethan Kyle Afrika-Peters <afrikapetersek@gmail.com>
2024-04-12 13:03:48 +0200

    update

--- project/index.js
+++ project/index.js
@@ -9,11 +9,6 @@ try {
     document.getElementById("author").textContent = `By: Dodi Achmad`
 }
 
-/**
- * Challenge: Update the code below and in the 
- * getCurrentLocation callback to use try...catch
- */
-
 try {
     const res = await fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     if (!res.ok) {
